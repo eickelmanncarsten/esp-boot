820783b1b83903378f82c97a72e9124754f01f7e
eickelmanncarsten <eickelmanncarsten@gmail.com>
2026-05-19 18:21:13 +0200

    Update custom_backlight.h

--- my_components/custom_backlight/custom_backlight.h
+++ my_components/custom_backlight/custom_backlight.h
@@ -12,43 +12,13 @@ class CustomBacklightOutput : public output::BinaryOutput, public Component {
   void set_pin_hub(pca9554::PCA9554Component *hub) { this->hub_ = hub; }
 
   void setup() override {
-    this->write_state(true); // Schaltet das Backlight initial ein
-  }
-
-  void write_state(bool state) override {
-    // KORREKTUR: pin_write statt digital_write
-    if (this->hub_ != nullptr) {
-        this->hub_->pin_write(5, state); 
-     #pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/components/output/binary_output.h"
-#include "esphome/components/pca9554/pca9554.h"
-
-namespace esphome {
-namespace custom_backlight {
-
-class CustomBacklightOutput : public output::BinaryOutput, public Component {
- public:
-  void set_pin_hub(pca9554::PCA9554Component *hub) { this->hub_ = hub; }
-
-  void setup() override {
     this->write_state(true);
   }
 
   void write_state(bool state) override {
-    // JETZT EXAKT: digital_write ist die richtige Methode für den PCA9554
     if (this->hub_ != nullptr) {
+        // Nutzt die korrekte, offizielle Methode der ESPHome PCA9554-Komponente
         this->hub_->digital_write(5, state); 
-    }
-  }
-
- protected:
-  pca9554::PCA9554Component *hub_{nullptr};
-};
-
-}  // namespace custom_backlight
-}  // namespace esphome
     }
   }
 
