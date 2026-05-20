2b6420dabec0e0a53d5ba1bf350e7428b0d45285
eickelmanncarsten <eickelmanncarsten@gmail.com>
2026-05-20 14:26:53 +0200

    Update custom_backlight.h

--- my_components/custom_backlight.h
+++ my_components/custom_backlight.h
@@ -1,22 +1,24 @@
 #pragma once
 #include "esphome.h"
 
-class CustomBacklightOutput : public esphome::output::BinaryOutput, public esphome::Component {
+class CustomBacklightOutput {
  private:
   esphome::Component *hub_{nullptr};
 
  public:
   void set_pin_hub(esphome::Component *hub) { this->hub_ = hub; }
 
-  void setup() override {
-    // Initialisierung falls nötig
-  }
-
-  void write_state(bool state) override {
+  void set_state(bool state) {
     if (state) {
       ESP_LOGD("backlight", "Backlight wird eingeschaltet...");
+      // Hier läuft dein originaler Code zum Einschalten über den Expander
     } else {
       ESP_LOGD("backlight", "Backlight wird ausgeschaltet...");
+      // Hier läuft dein originaler Code zum Ausschalten über den Expander
     }
   }
 };
+
+// Wir erstellen direkt eine globale Instanz, die wir im YAML ansprechen können
+extern CustomBacklightOutput my_bl;
+CustomBacklightOutput my_bl;
