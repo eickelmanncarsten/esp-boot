761019ddff4ef5271b34ece7daf21ebe741f2289
eickelmanncarsten <eickelmanncarsten@gmail.com>
2026-05-20 19:03:25 +0200

    Update my_custom_i2c.h

--- my_custom_i2c/my_custom_i2c.h
+++ my_custom_i2c/my_custom_i2c.h
@@ -21,26 +21,26 @@ class MyCustomI2CComponent : public Component {
     delay(100);
 
     // 2. Den echten I2C-Bus mit der Arduino Wire-Bibliothek initialisieren
-    Wire.begin(6, 7, 100000); 
+    ::Wire.begin(6, 7, 100000); 
     
     // 3. Den PCA9554 (0x20) direkt ansprechen und das Backlight einschalten
-    Wire.beginTransmission(0x20);
-    byte error = Wire.endTransmission();
+    ::Wire.beginTransmission(0x20);
+    byte error = ::Wire.endTransmission();
     
     if (error == 0) {
       ESP_LOGI("custom_i2c", "Erfolg! PCA9554 an 0x20 reagiert auf Arduino Wire!");
       
       // Pin 0 (Backlight) als Ausgang definieren
-      Wire.beginTransmission(0x20);
-      Wire.write(0x03); // Konfigurationsregister
-      Wire.write(0xFE); // Pin 0 = Output, Rest = Input
-      Wire.endTransmission();
+      ::Wire.beginTransmission(0x20);
+      ::Wire.write(0x03); // Konfigurationsregister
+      ::Wire.write(0xFE); // Pin 0 = Output, Rest = Input
+      ::Wire.endTransmission();
 
       // Pin 0 auf HIGH setzen (Backlight einschalten)
-      Wire.beginTransmission(0x20);
-      Wire.write(0x01); // Ausgangsregister
-      Wire.write(0x01); // Pin 0 auf HIGH
-      Wire.endTransmission();
+      ::Wire.beginTransmission(0x20);
+      ::Wire.write(0x01); // Ausgangsregister
+      ::Wire.write(0x01); // Pin 0 auf HIGH
+      ::Wire.endTransmission();
       ESP_LOGI("custom_i2c", "Backlight-Befehl erfolgreich gesendet!");
     } else {
       ESP_LOGE("custom_i2c", "PCA9554 hat nicht geantwortet. Fehlercode: %d", error);
