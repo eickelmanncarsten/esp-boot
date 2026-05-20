e0278802c73f5913918ed20bdb855317e720ec14
eickelmanncarsten <eickelmanncarsten@gmail.com>
2026-05-20 13:32:43 +0200

    Update __init__.py
    neu

--- my_components/custom_touch/__init__.py
+++ my_components/custom_touch/__init__.py
@@ -1,1 +1,15 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
+# Definiert den Namespace deiner C++ Klasse
+custom_touch_ns = cg.esphome_ns.namespace('custom_touch')
+CustomTouch = custom_touch_ns.class_('CustomTouch', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CustomTouch),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
