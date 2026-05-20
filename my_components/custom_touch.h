b290e1a7eff7c6ab1bcb52f137d181fc3116a6e7
eickelmanncarsten <eickelmanncarsten@gmail.com>
2026-05-20 14:32:33 +0200

    Update custom_touch.h
    neu

--- my_components/custom_touch.h
+++ my_components/custom_touch.h
@@ -4,7 +4,8 @@
 class CustomTouch : public esphome::Component, public esphome::i2c::I2CDevice {
  public:
   void setup() override {
-    this->set_i2c_bus(id(bus_a));
+    // Das '&' sorgt dafür, dass die Adresse des Busses übergeben wird
+    this->set_i2c_bus(&id(bus_a));
     this->set_i2c_address(0x38);
     ESP_LOGD("custom_touch", "Initialisiere FocalTech Touchscreen auf Adresse 0x%02X...", this->address_);
   }
