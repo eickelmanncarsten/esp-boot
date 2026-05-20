765742925151a6833189d399567a83ff73dbbeba
eickelmanncarsten <eickelmanncarsten@gmail.com>
2026-05-20 13:34:10 +0200

    Update custom_touch.h

--- my_components/custom_touch/custom_touch.h
+++ my_components/custom_touch/custom_touch.h
@@ -1,1 +1,15 @@
+#pragma once
+#include "esphome.h"
 
+// Der Namespace muss mit der __init__.py übereinstimmen
+namespace custom_touch {
+
+class CustomTouch : public esphome::Component, public esphome::i2c::I2CDevice {
+ public:
+  void setup() override {
+    ESP_LOGD("custom_touch", "Initialisiere FocalTech Touchscreen auf Adresse 0x%02X...", this->address_);
+  }
+  void loop() override {}
+};
+
+}  // namespace custom_touch
